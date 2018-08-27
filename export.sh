fcd0a36cb741fc268ef5f10673bdd89723a50922
URA Takefumi <ura.takefumi@gmail.com>
2018-08-28 01:02:35 +0900

    export.sh のバグを修正

--- export.sh
+++ export.sh
@@ -7,6 +7,7 @@ if $CU status 2>&1 | $G enabled > /dev/null; then
     echo Please set SIP disable.
     echo Read the readme.md for detail.
 else
-    cp "`sh cat-dir.sh KeySetting_Default.plist`" .
-    cp "`sh cat-dir.sh KeySetting_Windows.plist`" .
+    cp KeySetting_Default.plist "`sh cat-dir.sh`"
+    cp KeySetting_Windows.plist "`sh cat-dir.sh`"
+    killall -HUP JapaneseIM
 fi
